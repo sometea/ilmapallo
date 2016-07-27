c673a5c9f13778384728d4f78d7acf85bbd9bd1d
David Dasenbrook <david.dasenbrook@gmail.com>
2016-07-27 15:58:57 +0200

    add karma test runner

--- client/articles.component.spec.js
+++ client/articles.component.spec.js
@@ -10,7 +10,12 @@ describe('ArticlesComponent', () => {
   });
   
   it('holds a reference to the selected article', () => {
-    articlesComponent.onSelect({title: 'Test', text: 'Test text'});
-    expect(articlesComponent.selectedArticle).toEqual({title: 'Test', text: 'Test text'});
+    articlesComponent.onSelect({ title: 'Test', text: 'Test text' });
+    expect(articlesComponent.selectedArticle).toEqual({ title: 'Test', text: 'Test text' });
+  });
+
+  it('creates a new article', () => {
+    articlesComponent.onNew();
+    expect(articlesComponent.selectedArticle).not.toBe(null);
   });
 });
