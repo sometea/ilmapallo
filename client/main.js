76ef4283635ef8680d1b075429d8aa78394f82e6
David Dasenbrook <david.dasenbrook@gmail.com>
2016-08-28 17:26:58 +0200

    fix FormBuilder injection

--- client/main.js
+++ client/main.js
@@ -2,12 +2,13 @@
 
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { FormBuilder } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { APP_ROUTER_PROVIDER } from './router';
 
 const main = document.addEventListener('DOMContentLoaded', () => {
-  bootstrap(AppComponent, [HTTP_PROVIDERS, APP_ROUTER_PROVIDER]);
+  bootstrap(AppComponent, [HTTP_PROVIDERS, APP_ROUTER_PROVIDER, FormBuilder]);
 });
 
 export { main };
